89e331a31fa8dcdead122281d81ca0484d107cdc
TheMadClover <130436262+TheMadClover@users.noreply.github.com>
2024-07-30 00:17:16 -0700

    madbroom original

---
+++ keyboards/handwired/themadclover/madbroom/config.h
@@ -0,0 +1,46 @@
+// Copyright 2024 TheMadClover (@TheMadClover)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 5
+
+#define DIRECT_PINS { \
+    { GP13, GP28, GP12, GP29, GP0 }, \
+    { GP22, GP14, GP26, GP4,  GP27}, \
+    { GP21, GP23, GP7, GP20, GP6 }, \
+    { GP16, GP9, GP8, NO_PIN, NO_PIN } \
+}
+
+#define DIRECT_PINS_RIGHT {  \
+    { GP0, GP29, GP12, GP28, GP13 }, \
+    { GP27, GP4, GP26, GP14, GP22}, \
+    { GP6, GP20, GP7, GP23, GP21 }, \
+    { GP8, GP9, GP16, NO_PIN, NO_PIN } \
+}
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* Serial Config */
+#define USE_SERIAL
+#define SERIAL_USART_TX_PIN GP1
+#define MASTER_LEFT
+
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
